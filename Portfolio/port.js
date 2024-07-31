492ce5131a42e079c3668d133bc4c7e950415655
Rushikesh54 <78204316+Rushikesh54@users.noreply.github.com>
2024-07-31 20:56:14 +0530

    Add files via upload

---
+++ Portfolio/port.js
@@ -0,0 +1,9 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('resume');
+
+   // button.addEventListener('click', function() {
+        window.location.href = 'Rushikesh Shirwalkar CV.pdf';
+    });
+});
+
+*/
